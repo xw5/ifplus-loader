83a5078988aba66aa4598670af2171476f75e736
xiewu <xiewu@ygsoft.com>
2021-03-04 20:31:15 +0800

    loader初始化

---
+++ conditionalConst.js
@@ -0,0 +1,52 @@
+// 正则集合
+exports.regexList = [{
+  type: "ifndef",
+  test: /<!-- #ifndef (\w+?) -->([\w\W]+?)<!-- #endif -->/
+},
+{
+  type: "ifdef",
+  test: /<!-- #ifdef (\w+?) -->([\w\W]+?)<!-- #endif -->/
+},
+{
+  type: "ifndef",
+  test: /\/\* #ifndef (\w+?) \*\/([\w\W]+?)\/\* #endif \*\//
+},
+{
+  type: "ifdef",
+  test: /\/\* #ifdef (\w+?) \*\/([\w\W]+?)\/\* #endif \*\//
+},
+{
+  type: "ifndef",
+  test: /\/\/ #ifndef (\w+?) ([\w\W]+?)\/\/ #endif/
+},
+{
+  type: "ifdef",
+  test: /\/\/ #ifdef (\w+?) ([\w\W]+?)\/\/ #endif/
+}];
+
+// 条件类型
+exports.types = {
+  "ifndef": 'ifndef',
+  "ifdef": "ifdef"
+}
+// /<!--  #ifndef (\w+) -->([\w\W]+)<!--  #endif -->/, 
+// /<!--  #ifdef (\w+) -->([\w\W]+)<!--  #endif -->/,
+// /\/\*  #ifndef  (\w+)  \*\/([\w\W]+)\/\*  #endif  \*\//,
+// /\/\*  #ifdef  (\w+)  \*\/([\w\W]+)\/\*  #endif  \*\//,
+// /\/\/ #ifndef  (\w+)([\w\W]+)\/\/ #endif/,
+// /\/\/ #ifdef  (\w+)([\w\W]+)\/\/ #endif/
+
+// html条件判断
+// <!-- #ifdef %PLATFORM% -->
+// 平台特有的组件
+// <!-- #endif -->
+
+// css条件判断
+// /* #ifdef %PLATFORM%  */
+// 平台特有样式
+// /* #endif  */
+
+// js条件判断
+// // #ifdef %PLATFORM%
+// 平台特有的API实现
+// // #endif
---
+++ index.js
@@ -0,0 +1,52 @@
+const process = require("process");
+const { regexList, types} = require("./conditionalConst.js");
+
+module.exports = function (source) {
+  // 获取命令行参数
+  const { argv } = process;
+  const conditionalItem = argv.find((item) => {
+    return item.indexOf("--compiler=") != -1;
+  });
+
+  // 如果没有传任务关于条件判断相关的参数则不做任务处理返回源数据
+  if (!conditionalItem) {
+    return source;
+  }
+
+  // 获取条件判断关键字
+  let conditionalKey = conditionalItem.split("=")[1];
+  
+  // 递归的正则匹配
+  function matchRegex(item) {
+    let matchs = source.match(item.test);
+    // console.log("matchRegex", conditionalKey, matchs);
+    if (!matchs) {
+      return;
+    }
+
+    // 处理仅在某平台存在的情况
+    if (item.type == types.ifndef) {
+      if (matchs[1].indexOf(conditionalKey) !== -1) {
+        source = source.replace(matchs[0], '');
+      } else {
+        source = source.replace(matchs[0], matchs[2]);
+      }
+    }
+
+    // 处理除了某平台均存在的情况
+    if (item.type == types.ifdef) {
+      if (matchs[1].indexOf(conditionalKey) === -1) {
+        source = source.replace(matchs[0], '');
+      } else {
+        source = source.replace(matchs[0], matchs[2]);
+      }
+    }
+    item.test.lastIndex = 0;
+    matchRegex(item);
+  }
+
+  regexList.forEach((item) => {
+    matchRegex(item);
+  });
+  return source;
+}
